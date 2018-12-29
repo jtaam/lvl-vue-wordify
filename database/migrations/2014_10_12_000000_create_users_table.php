b0115ec348ce64c4cb6d80757a68f930816edb91
_tam <tamjid84@yahoo.com>
2018-12-29 13:12:03 +0600

    move User.php to Models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('bio')->nullable();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('role')->default('subscriber');
+            $table->string('avatar')->default('avatar.png');
             $table->rememberToken();
             $table->timestamps();
         });
